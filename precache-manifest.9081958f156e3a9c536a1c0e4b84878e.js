39a149cd6210bbe128d551b8027e30ba1210f441
Sam <smn729@163.com>
2019-04-12 17:36:38 +0800

    Updates

---
+++ precache-manifest.9081958f156e3a9c536a1c0e4b84878e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gooapp/web/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1f7e6876e2a3e549a9e0",
+    "url": "/gooapp/web/static/js/runtime~main.12820056.js"
+  },
+  {
+    "revision": "f18ff9322fe360d9bbc9",
+    "url": "/gooapp/web/static/js/main.e1df11ff.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/gooapp/web/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "f18ff9322fe360d9bbc9",
+    "url": "/gooapp/web/static/css/main.1a206faa.chunk.css"
+  },
+  {
+    "revision": "15096d5f6251deca029d767e062bcca8",
+    "url": "/gooapp/web/index.html"
+  }
+];
