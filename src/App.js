a45166b8f5095fd988dcb46f8ece78fdbd0fbd16
Sam <smn729@163.com>
2019-05-16 22:07:10 +0800

    up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 import MainPage from './Pages/MainPage';
+import Mzsm from './Pages/Mzsm';
+import { Link, Switch, BrowserRouter, Route } from 'react-router-dom';
+import Bqbhzy from './Pages/Bqbhzy';
+import Ysqzc from './Pages/Ysqzc';
+import Rjxkxy from './Pages/Rjxkxy';
+
+
+function Home ()
+{
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About ()
+{
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
 
 class App extends Component
 {
@@ -12,11 +36,19 @@ class App extends Component
   render ()
   {
     return (
-      <div className="App">
-        <MainPage />
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/mianZheShengMing" component={ Mzsm } />
+          <Route path="/banQuanBaoHuZhiYin" component={ Bqbhzy } />
+          <Route path="/yinSiQuanZhengCe" component={ Ysqzc } />
+          <Route path="/ruanJianXuKeXieYi" component={ Rjxkxy } />>
+          <Route path="/" component={ MainPage } />
+        </Switch>
+      </BrowserRouter>
     );
   }
+
+
 }
 
 export default App;
